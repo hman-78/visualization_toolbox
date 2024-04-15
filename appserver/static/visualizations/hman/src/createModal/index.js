ffc769debe20c3a18e09d802c58d7fcce6ecbb52
Octavian Heraeus <heraeus@selardi.com>
2024-04-15 11:23:55 +0200

    Isolate initialize logic

---
+++ appserver/static/visualizations/hman/src/createModal/index.js
@@ -0,0 +1,198 @@
+function _createModal(splunk) {
+  var h2 = $("<h2>").text("Add Annotation");
+  var xValueSpan = $("<span>").addClass("x-value");
+  var yValueSpan = $("<span>").addClass("y-value");
+  var modal_annotationContent = $("<div>").addClass("modal_annotation-content").append(header, body, footer);
+
+  var opcoContainer = $("<div>").addClass("value-container").attr("id", "opcoContainer").css("display", "none");;
+  $(".modal_annotation-content").append(opcoContainer);
+
+  var annotationSeriesName = $("<div>").addClass("annotation-series-name").attr("id", "annotationSeriesNameContainer").css("display", "none");
+  $(".modal_annotation-content").append(annotationSeriesName);
+
+  var opcoElement = $("<div>")
+    .addClass("opco")
+    .attr("id", "opcoContainer").css("display", "none");
+
+  //var xValueContainer = $("<span>")
+  var xValueContainer = $("<div>")
+
+    .addClass("x-value-container")
+    .text("Current X Value: ")
+    //.append($("<span>").addClass("x-value")); // This span will hold the current x-axis value
+    //.append(xValueSpan);
+    .append(xValueSpan)[0]; // Get the DOM element from jQuery object
+  //.attr("id", "xValueContainer"); // Add ID to the xValueContainer element
+  xValueContainer.id = "xValueContainer"; // Add ID to the xValueContainer element
+
+  var xValueElement = $("<div>").attr("id", "xValue").css("display", "none");;
+  var xAxisValueElement = $("<div>").attr("id", "xAxisValue").css("display", "none");;
+
+
+  //var yValueContainer = $("<span>")
+  var yValueContainer = $("<div>")
+    .addClass("y-value-container")
+    .text("Current Y Value: ")
+    //.append(yValueSpan);
+    //.attr("id", "yValueContainer"); // Add ID to the yValueContainer element
+    .append(yValueSpan)[0]; // Get the DOM element from jQuery object
+  yValueContainer.id = "yValueContainer"; // Add ID to the yValueContainer element
+  var yValueElement = $("<div>").attr("id", "yValue").css("display", "none");;
+
+
+  function clearModalData() {
+    descriptionInput.val("");
+    $(".modal_annotation").css("display", "none");
+    document.getElementById("descriptionInput").value = "";
+    document.getElementById("xValueContainer").textContent = "";
+    document.getElementById("yValueContainer").textContent = "";
+  }
+
+  var closeSpan = $("<span>")
+    .addClass("close")
+    .click(clearModalData);
+
+  var header = $("<div>")
+    .addClass("write-the-discription")
+    .append(closeSpan, h2, xValueContainer, yValueContainer, opcoElement, annotationSeriesName, xValueElement, xAxisValueElement, yValueElement);
+
+
+  var p = $("<p>").text("Description");
+  var descriptionInput = $("<input>")
+    .attr("type", "text")
+    .attr("placeholder", "Enter Description").attr("id", "descriptionInput").attr("autocomplete", "off").attr("class", "wide-input");
+
+
+  // var body = $("<div>").addClass("modal_annotation-body").append(p, descriptionInput, keyLabel, keyInput);
+  var body = $("<div>").addClass("modal_annotation-body").append(p, descriptionInput);
+
+
+  var cancelButton = $("<button>")
+    .attr("id", "cancelButton")
+    .text("Cancel")
+    .addClass("shadow-button")
+    .click(clearModalData);
+
+
+
+  var saveButton = $("<button>")
+    .attr("id", "saveButton")
+    .text("Save")
+    .addClass("shadow-button")
+    .css({
+      "background-color": "#32CD32",
+      "border-color": "#32CD32"
+    });
+
+
+
+  var footer = $("<div>").addClass("modal_annotation-footer").append(cancelButton, saveButton);
+
+  var content = $("<div>").addClass("modal_annotation-content").append(header, body, footer);
+  var modal_annotation = $("<div>")
+    .addClass("modal_annotation")
+    .attr("id", "myModal_annotation")
+    .css("display", "none")
+    .append(content);
+
+  $("body").append(modal_annotation);
+
+  document.getElementById("saveButton").addEventListener("click", function (splunk) {
+    return function () {
+
+
+      var descriptionInput = document.getElementById("descriptionInput");
+      var description = descriptionInput.value;
+      var annotationSeriesName = document.getElementById("annotationSeriesNameContainer").textContent;
+      var opco = document.getElementById("opcoContainer").textContent;
+      var xValue = document.getElementById("xValue").textContent;
+      var xAxisValue = document.getElementById("xAxisValue").textContent;
+      var yValue = document.getElementById("yValue").textContent;
+
+
+      var msgJson = {
+        "type": "annotation",
+        "action": "add",
+        "opco": opco,
+        "name": annotationSeriesName,
+        "x": xValue,
+        "annotation": description,
+        "tags": ""
+      };
+
+
+      splunk._sendMQTTMessage(JSON.stringify(msgJson));
+
+      for (var i = 0; i < _data.rows.length; i++) {
+        var x = _data.rows[i][_annotationSeriesDataIndex[0]];
+        var y = _data.rows[i][_annotationSeriesDataIndex[1]];
+        var annotation = _data.rows[i][_annotationSeriesDataIndex[2]];
+
+        // to avoid a refresh of the panel the annotation with the matching x value is updated with the new annoation
+        if (xAxisValue == x) {
+          _data.rows[i][_annotationSeriesDataIndex[2]] = description;
+        }
+
+      }
+      var isAnnotationAlreadyInData = false;
+      var indexToBeDeleted = null;
+      for (var i = 0; i < _option.series[_annotationSeriesIndex].data.length; i++) {
+        var obj = _option.series[_annotationSeriesIndex].data[i];
+        var x = obj[0];
+        if (xAxisValue == x) {
+          isAnnotationAlreadyInData = true;
+          if ("" == description) {
+            // remove data from series
+            indexToBeDeleted = i;
+            obj[2] = "";
+            _option.series[_annotationSeriesIndex].data.splice(i, 1);
+            // TODO remove empty data obj from array
+          } else {
+            // update new value
+            obj[2] = description;
+          }
+        }
+
+      }
+
+      if (!isAnnotationAlreadyInData) {
+        var obj = [];
+        obj.push(xAxisValue);
+        obj.push(yValue);
+        obj.push(description);
+        _option.series[_annotationSeriesIndex].data.push(obj);
+      }
+      _myChart.setOption(_option);
+
+
+      // Hide the modal_annotation after saving
+      $(".modal_annotation").css("display", "none");
+      // reset description
+      document.getElementById("descriptionInput").value = "";
+      document.getElementById("xValueContainer").text = "";
+      document.getElementById("yValueContainer").text = "";
+
+
+    };
+  }(splunk));
+
+  document.getElementById("descriptionInput").addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      // Call the click listener of the saveButton when Enter key is pressed
+      document.getElementById("saveButton").click();
+    }
+  });
+
+  // When the user clicks anywhere outside of the modal_annotation, close it
+  window.onclick = function (event) {
+    var modal_annotationElement = document.getElementById("myModal_annotation");
+    if (event.target == modal_annotationElement) {
+      modal_annotationElement.style.display = "none";
+      document.getElementById("descriptionInput").value = "";
+      document.getElementById("xValueContainer").text = "";
+      document.getElementById("yValueContainer").text = "";
+    }
+  }
+}
+
+module.exports = _createModal;
---
+++ appserver/static/visualizations/hman/src/handleAnnotation/index.js
@@ -0,0 +1,155 @@
+function _handleAnnotation(data, config, option, annotationsSeriesName, myChart) {
+
+  var configXAxisDataIndexBinding = config[this.getPropertyNamespaceInfo().propertyNamespace + "xAxisDataIndexBinding"];
+  var configSeriesDataIndexBinding = config[this.getPropertyNamespaceInfo().propertyNamespace + "seriesDataIndexBinding"];
+  var opco = config[this.getPropertyNamespaceInfo().propertyNamespace + "opco"];
+  // first index points to x, second to y and third to the index of the annotation
+  var annotationSeriesDataIndexBinding = config[this.getPropertyNamespaceInfo().propertyNamespace + "annotationSeriesDataIndexBinding"];
+
+  var annotationSeriesDataIndex = [];
+  annotationSeriesDataIndex = this._parseIndex(annotationSeriesDataIndexBinding);
+  _annotationSeriesDataIndex = annotationSeriesDataIndex;
+
+  if (annotationSeriesDataIndex.length != 3) {
+    var error = "Please provide the configuration annotationSeriesDataIndexBinding. It has to be 3 numbers separated by a comma.\n"
+    var error = error + "1. Index of the column providing the x-value of the annotated series.\n"
+    var error = error + "2. Index of the column providing the y-value of the annotated series.\n"
+    var error = error + "3. Index of the column providing the annotation value.\n"
+    var error = error + "Example: 0,1,2\n"
+    var error = error + "0 indicates that the first column is used as the x-value of the annotation.\n"
+    var error = error + "1 indicates that the second column is used as the y-value of the annotation.\n"
+    var error = error + "2 indicates that the third column providing the value.\n"
+
+    throw error
+  }
+  // we use the DOM tree to store some values to make them available in the click listeners
+  document.getElementById("annotationSeriesNameContainer").textContent = annotationsSeriesName;
+  document.getElementById("opcoContainer").textContent = opco;
+
+  myChart.on('click', (function (option) {
+    return function (params) {
+
+      var annotationSeriesName = document.getElementById("annotationSeriesNameContainer").textContent;
+
+
+      if (annotationSeriesName != null) {
+        var xValue;
+        var xAxisValue;
+        var yValue = params.value;
+        if (params.seriesName === annotationSeriesName) {
+
+          if (option === null || option.xAxis === null || !Array.isArray(option.xAxis) ||
+            option.xAxis.length == 0 || option.xAxis[0].type === null) {
+            throw new Error("xAxis.type missing in option");
+          }
+          // according to the echart documentation the x value has to be read from different
+          // attributes of the params depending on the type of the x Axis
+          if (option.xAxis[0].type === "time") {
+            // x Axis of type time is an expeptional case. We get unix time in the search
+            // but splunk converts it to a readable format based on the local time of the user
+            // We would run into problems if the local time would be used as part of the key in 
+            // the database. The solution is to store the unix time instead of the local time.
+            // This is the reason why the user has to provide the unix time in the dimension "value"
+            // of the x axis data.
+            if (Array.isArray(params.dimensionNames) && params.dimensionNames.length > 0) {
+              for (var i = 0; i < params.dimensionNames.length; i++) {
+                if ("x" === params.dimensionNames[i]) {
+                  xAxisValue = params.value[i];
+                } else if ("value" === params.dimensionNames[i]) {
+                  xValue = params.value[i];
+                }
+              }
+            }
+          } else if (option.xAxis[0].type === "category") {
+            xValue = params.name;
+            xAxisValue = xValue;
+
+          } else if (option.xAxis[0].type === "value") {
+            if (Array.isArray(params.dimensionNames) && params.dimensionNames.length > 0) {
+              for (var i = 0; i < params.dimensionNames.length; i++) {
+                if ("x" === params.dimensionNames[i]) {
+                  xValue = params.value[i];
+                  xAxisValue = xValue;
+                }
+              }
+            }
+          }
+
+          if (Array.isArray(params.dimensionNames) && params.dimensionNames.length > 0) {
+            for (var i = 0; i < params.dimensionNames.length; i++) {
+              if ("y" === params.dimensionNames[i]) {
+                yValue = params.value[i];
+              }
+            }
+          }
+
+          // store hidden values in the DOM tree
+          document.getElementById("xValue").textContent = xValue;
+          document.getElementById("xAxisValue").textContent = xAxisValue;
+          document.getElementById("yValue").textContent = yValue;
+
+          var descriptionInput = document.getElementById("descriptionInput");
+          descriptionInput.focus();
+          descriptionInput.select();
+
+          for (var i = 0; i < _option.series[_annotationSeriesIndex].data.length; i++) {
+            var obj = _option.series[_annotationSeriesIndex].data[i];
+            var x = obj[0];
+            if (xAxisValue == x) {
+              var description = obj[2];
+              descriptionInput.value = description;
+            }
+
+          }
+
+          $("#xValueContainer").text("X Value: " + xAxisValue); // Update X value in the modal_annotation
+          $("#yValueContainer").text("Y Value: " + yValue); // Update Y value in the modal_annotation
+          var modal_annotationElement = document.getElementById("myModal_annotation")
+          modal_annotationElement.style.display = "block";
+        }
+      }
+    }
+  })(option));
+
+  if (annotationSeriesDataIndexBinding == null) {
+    var error = "Missing configuration. Please provide a config named 'annotationSeriesDataIndexBinding' with the index of the column that contains the annotation data.";
+    throw error;
+  } else {
+    var annotationSeries = {
+      type: 'scatter',
+      symbol: 'pin',
+      symbolSize: 15,
+      data: [],
+      itemStyle: {
+        color: '#ff6600', // Set the color of the annotation symbols
+        opacity: 1, // Adjust the opacity of the annotation symbols
+        borderColor: '#000', // Set the border color of the annotation symbols
+        borderWidth: 1 // Set the border width of the annotation symbols
+      },
+      label: {
+        show: true, // Display the annotation text
+        formatter: function (param) {
+          return param.value[2]; // Show the annotation text
+        },
+        position: 'top' // Position the annotation text above the symbols
+      }
+    };
+    annotationSeries["data"] = [];
+    annotationSeries["name"] = annotationsSeriesName + " annotation";
+    for (var i = 0; i < data.rows.length; i++) {
+      var annotationValue = data.rows[i][annotationSeriesDataIndex[2]];
+      if (annotationValue != null && "" != annotationValue && "'-'" != annotationValue) {
+        var annotationData = [];
+        annotationData.push(data.rows[i][annotationSeriesDataIndex[0]]);
+        annotationData.push(data.rows[i][annotationSeriesDataIndex[1]]);
+        annotationData.push(data.rows[i][annotationSeriesDataIndex[2]]);
+        annotationSeries.data.push(annotationData);
+      }
+    }
+    _annotationSeriesIndex = option.series.length;
+    option.series.push(annotationSeries);
+  }
+
+}
+
+module.exports = _handleAnnotation;
