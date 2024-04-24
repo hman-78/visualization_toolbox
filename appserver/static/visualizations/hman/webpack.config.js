72dbfb26c313b605778a973d9adacab278d95b70
Octavian Heraeus <heraeus@selardi.com>
2024-04-24 11:20:45 +0200

    Update webpack config

--- appserver/static/visualizations/hman/webpack.config.js
+++ appserver/static/visualizations/hman/webpack.config.js
@@ -8,8 +8,13 @@ module.exports = {
     },
     mode: 'production',
     output: {
+        path: `${__dirname}`,
         filename: 'visualization.js',
         libraryTarget: 'amd'
+    },
+    //Temporary flag until we test properly the changes
+    optimization: {
+        minimize: false
     },
     externals: [
         'api/SplunkVisualizationBase',
