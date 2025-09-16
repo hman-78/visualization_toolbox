58fa32e66d465a8797e81a2f3cd3fc77b18388d0
domselardi <132788754+domselardi@users.noreply.github.com>
2025-09-16 11:59:14 +0200

    Update index.js

--- appserver/static/visualizations/hman/src/updateView/index.js
+++ appserver/static/visualizations/hman/src/updateView/index.js
@@ -42,14 +42,16 @@ const _updateView = function (data, config) {
   this.scopedVariables['_renderedEchartsArray'].push(tmpChart);
 
   let option = {};
-  if (echartProps.dataType == "Custom") {
+  if (echartProps.dataType.toLowerCase() == "custom") {
     option = this._buildCustomOption(data, config);
-  } else if (echartProps.dataType == "Boxplot") {
+  } else if (echartProps.dataType.toLowerCase() == "boxplot") {
     option = this._buildBoxplotOption(data, config);
-  } else if (echartProps.dataType == "SimpleBoxplot") {
+  } else if (echartProps.dataType.toLowerCase() == "simpleboxplot") {
     option = this._buildSimpleBoxplotOption(data, config);
-  } else if (echartProps.dataType == "Timeline") {
+  } else if (echartProps.dataType.toLowerCase() == "timeline") {
     option = this._buildTimelineOption(data, config, tmpChart['instanceByDom']);
+  } else if (echartProps.dataType.toLowerCase() == "hourlytimeline") {
+    option = this._buildHourlyTimelineOption(data, config, tmpChart['instanceByDom']);
   }
   // tokens might not yet be replaced in the option. In this case we
   // don't want the echart to be shown yet, as it would result in an error.
